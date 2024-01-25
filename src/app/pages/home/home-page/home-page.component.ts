4a1a0b318eaeda48a6d55c055def872ae1c5b940
janith-rathanyaka <mnjrathnayaka97@yahoo.com>
2024-01-25 14:00:40 +0530

    add product ui

--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductViewCardComponent } from '../../../share/cards/product-view-card/product-view-card.component';
 import { MainSliderComponent } from './inner-items/main-slider/main-slider.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [MainSliderComponent],
+  imports: [MainSliderComponent,ProductViewCardComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
